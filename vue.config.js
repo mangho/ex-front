d494300145f253fdf6ad649233a47db18ee50d22
jaydon007 <jaydon007@live.com>
2019-01-10 11:27:09 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'?'/testv/':'/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/testv/' : '/',
     devServer: {
         port:8090,
         open:true
