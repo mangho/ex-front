473bb73adedfa304aa39183c11b512f52deb930e
jaydon007 <jaydon007@live.com>
2019-01-17 19:11:06 +0800

    update route

--- src/mock/db.js
+++ src/mock/db.js
@@ -5,13 +5,9 @@ module.exports = function () {
     var data = {
         news: []
     };
-
     var images = [1, 2, 3].map(x => Random.image('200x100', Random.color(), Random.word(2, 6)));
-
-    for (var i = 0; i < 100; i++) {
-
+    for (var i = 0; i < 10; i++) {
         var content = Random.cparagraph(0, 10);
-
         data.news.push({
             id: i,
             title: Random.cword(8, 20),
@@ -21,6 +17,5 @@ module.exports = function () {
             images: images.slice(0, Random.integer(1, 3))
         })
     }
-
     return data
 }
