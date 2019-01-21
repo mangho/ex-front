9566932d4f1d6d501d0a50bae00049df127d88ff
jaydon007 <jaydon007@live.com>
2019-01-21 11:26:43 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
     </v-navigation-drawer>
 
     <div class="hide-overflow" style="position: relative;">
-      <v-toolbar color="indigo" fixed dark>
+      <v-toolbar color="deep-purple" fixed dark>
         <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
         <router-link to="/"><v-img :src="require('@/assets/logo.svg')" class="my-1" contain height="40" width="40"></v-img></router-link>
         <v-toolbar-title>Title</v-toolbar-title>
